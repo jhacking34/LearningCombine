073838a61797339fda0055a3791e3b0ae0719b93
Johnny Hacking <jhacking@bhico.com>
2021-06-11 07:04:21 -0600

    Initial Commit

---
+++ LearningCombine/LearningCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningCombineApp.swift
+//  LearningCombine
+//
+//  Created by Johnny Hacking on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
