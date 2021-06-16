b75615d8fe637a1cde96fffd1699c354acb4a84c
Johnny Hacking <jhacking@bhico.com>
2021-06-16 08:30:19 -0600

    update to error handling

--- LearningCombine/ViewModel/EmployeeProfileViewModel.swift
+++ LearningCombine/ViewModel/EmployeeProfileViewModel.swift
@@ -16,10 +16,13 @@ class EmployeeProfileViewModel : ObservableObject{
         let url = URL(string: "https://api.bhico.com/api/v2/swifti/_table/beta?api_key=185fbe051e8c1f312afa7c80aa0f2b4a9506d7a24dec0b9da5e326e85198e714")!
         URLSession.shared.dataTaskPublisher(for: url)
             .map { $0.data}
+        // map has Data task publisher error handling
             .decode(type: EmployeeResponse.self, decoder: JSONDecoder())
             .receive(on: RunLoop.main)
             .sink(receiveCompletion: { completion in
                 if case .failure(let error) = completion {
+                    
+                    //Decode Error
 //                    switch error {
 //                    case .invalidResponse:
 //                        print("bad")
