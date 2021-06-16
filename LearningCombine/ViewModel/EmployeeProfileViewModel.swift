a0e12b2d855abe8b52e8e823c371267052dca385
Johnny Hacking <jhacking@bhico.com>
2021-06-16 07:58:13 -0600

    basic error handling

--- LearningCombine/ViewModel/EmployeeProfileViewModel.swift
+++ LearningCombine/ViewModel/EmployeeProfileViewModel.swift
@@ -13,13 +13,17 @@ class EmployeeProfileViewModel : ObservableObject{
     var cancellables: Set<AnyCancellable> = []
     
     func fetch(){
-        let url = URL(string: "https://api.bhico.com/api/v2/swift/_table/beta?api_key=185fbe051e8c1f312afa7c80aa0f2b4a9506d7a24dec0b9da5e326e85198e714")!
+        let url = URL(string: "https://api.bhico.com/api/v2/swifti/_table/beta?api_key=185fbe051e8c1f312afa7c80aa0f2b4a9506d7a24dec0b9da5e326e85198e714")!
         URLSession.shared.dataTaskPublisher(for: url)
             .map { $0.data}
             .decode(type: EmployeeResponse.self, decoder: JSONDecoder())
             .receive(on: RunLoop.main)
             .sink(receiveCompletion: { completion in
-                print(completion)
+                if case .failure(let error) = completion{
+                    print(error.localizedDescription)
+                }else{
+                    print(completion)
+                }
             }, receiveValue: { [unowned self] employee in
                 employees = employee.resource
             })
