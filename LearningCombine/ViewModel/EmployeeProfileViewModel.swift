6c9cbd33f1f0efe16ced6947cac8d62733a27ce4
Johnny Hacking <jhacking@bhico.com>
2021-06-17 20:31:17 -0600

    Got the generic combine network call working

--- LearningCombine/ViewModel/EmployeeProfileViewModel.swift
+++ LearningCombine/ViewModel/EmployeeProfileViewModel.swift
@@ -15,14 +15,23 @@ class EmployeeProfileViewModel : ObservableObject{
     var cancellables: Set<AnyCancellable> = []
     
     func loadData(){
-        let url = URL(string: "https://api.bhico.com/api/v2/1swift/_table/beta?api_key=185fbe051e8c1f312afa7c80aa0f2b4a9506d7a24dec0b9da5e326e85198e714")!
+        let url = URL(string: "https://api.bhico.com/api/v2/swift/_table/beta?api_key=185fbe051e8c1f312afa7c80aa0f2b4a9506d7a24dec0b9da5e326e85198e714")!
         
-        fetch(url, defaultValue: EmployeeResponse.default) {
-            self.employees = $0.resource
-        }
+
+        fetch(url, defaultValue: EmployeeResponse.default, completed: { [self] result in
+            DispatchQueue.main.async {
+                switch result{
+                case .success(let employeesRes):
+                    self.employees = employeesRes.resource
+                case .failure(let error):
+                    self.errorForAlert = ErrorForAlert(message: "Details: \(error.rawValue)")
+                }
+            }
+            
+        })
     }
     
-    func fetch<T: Decodable, U: Error>(_ url: URL, defaultValue: T, _ networkUrlError: U, completion: @escaping (T) -> Void){
+    func fetch<T: Decodable>(_ url: URL, defaultValue: T, completed: @escaping (Result<T, UrlResponseErrors>) -> Void){
         let decoder = JSONDecoder()
         
         URLSession.shared.dataTaskPublisher(for: url)
@@ -39,21 +48,28 @@ class EmployeeProfileViewModel : ObservableObject{
                 return data
             }
             .decode(type: T.self, decoder: decoder)
-            .mapError{ error -> Never in
-                    if let responseError = error as? NetError {
+            .mapError{ error -> UrlResponseErrors in // This mapError is looking to return a UrlResponseError type.
+                            //This right here is check the error and trys to unwrap the optional as a UrlResponseErrors.  If the error is not that type it means its a different and runs the else
+                            if let responseError = error as? UrlResponseErrors {
+                                print(error)
                                 return responseError
                             } else {
+                                print(error)
+                                // We return the UrlResponseErrors.decodeError because that is what this closure is looking for and we do that cuz this else statement got ran because it
+                                // was not of type UrlResponseErrors AKA URL Error and so we assume its a different error dealing with the decode. so we return the generic decode error we built in that model.
                                 return UrlResponseErrors.decodeError
                             }
                         }
-            .receive(on: RunLoop.main)
             .sink(receiveCompletion: { completion in
                                 if case .failure(let error) = completion {
                                 // This inserts the error into the Error alert message and pulls that message from our UrlResponseErrors Model
-                                    self.errorForAlert = ErrorForAlert(message: "Details: \(error.rawValue)")
+                                    completed(.failure(error))
                                 }
                 
-                            },receiveValue: completion)
+                            }, receiveValue: { [unowned self] employee in
+                               // employees = employee //look at the employee model to see why we used employee.resource
+                                completed(.success(employee))
+                            })
             .store(in: &cancellables)
     }
     
